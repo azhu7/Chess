4c94447ec6b7998d77e96bd38e27441fda1d6285
Alex <Alex@DESKTOP-Q5SC353>
2017-03-31 17:01:37 -0400

    refurbish sync

--- src/player.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/player.h"
-
-std::ostream &operator<<(std::ostream &os, Player p) {
-    return os << static_cast<std::underlying_type<Player>::type>(p);
-}
--- src/tile.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/tile.h"
-
-std::ostream &operator<<(std::ostream &os, const Tile &pos) {
-    return os << (char)(pos.col + 'a') << pos.row + 1;
-}
